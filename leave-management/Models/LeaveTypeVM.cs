1f5bd482ac855a44ac10e97a3775952e749df123
gdvillarroel <gdvillarroel@ITEM-S84863>
2021-06-11 14:16:13 +0200

    Agregar archivos de proyecto.

---
+++ leave-management/Models/LeaveTypeVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class DetailsLeaveTypeVM
+    {
+        //We create a LeaveTypeVM similar to the LeaveType in the Data folder so we can have more control
+        //over what we want to show about this entity. Sometimes there could be information in the database
+        //that we don't want to include in our ViewModel
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeVM
+    {
+        [Required]
+        public string Name { get; set; }
+        
+    }
+}
---
+++ leave-management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,56 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveType entity)
+        {
+            _db.LeaveTypes.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+           return _db.LeaveTypes.ToList();
+        }
+
+        public LeaveType FindById(int id)
+        {
+            return _db.LeaveTypes.Find(id);
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            bool changes = _db.SaveChanges() > 0; //This returns the amount of records that were editted
+            return changes;
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            return Save();
+        }
+    }
+}
